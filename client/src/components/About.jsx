7f0f88a6c298a227dedf8c22d6405bd5deb702d7
Chimezie Anyaoha <Chim@Chimezies-MacBook-Pro.local>
2018-01-08 04:26:16 -0500

    edited the about syntax

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -6,17 +6,15 @@ class About extends Component {
         return (
             <div>
                 <h1>About Us</h1>
-                <h3 className='aboutText'>CMER is a community playlist app. Anyone can add their favorite song 
-                link to our playlist, so we can all enjoy it together. Our app takes a 
+                <h3 className='aboutText'>My Playlist is a community playlist app. Anyone can add their favorite song 
+                link to our playlist, so we can all enjoy it together. This app takes a 
                 persons' song link and queues it to our player. You have the option 
-                to delete or edit your song if you would like to. (*WARNING* PLEASE 
-                DON'T DELETE ANYONE'S ELSE SONG! THANK YOU) CMER is a project made by 
-                a group from the WDI Ada class at General Assembly. It is a work in 
-                progress so be warned tht some features may not work as intended or at 
-                all. If you have any questions regarding this project (or just want to 
-                shoot the breeze), feel free to visit my <a href='https://www.github.com/MadelineNF'>github</a>! 
-                This project was created in collaboration with Chim, Madeline, Emel, and Robert.
-                </h3>
+                to delete or edit your song if you would like to. My Playlist is a project orignally made by 
+                I myself (Chimezie Anyaoha), Madeline, Emel but it has been recently abanded and I decided to expand on the project. The original
+                githun link is <a href='https://github.com/chimeziaya3/CMER'>here</a> It is a work in 
+                progress so be warned tht some features may not work as intended just yet.
+                </h3> 
+    
             </div>
         )
     }
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import '../App.css'
 
 class Hero extends Component {
   render() {
     return (
-      <div id="hero"> <h1 className="hero-text">Welcome to OUR PLAYLIST!</h1> </div>
+      <div id="hero"> 
+        <h1 className="hero-text">Welcome to My PLAYLIST!</h1>
+        <h3 className="subtitle"> To go straight to the app just click on 'player' on the upper right corner </h3> 
+      </div>
     );
   };
 }
