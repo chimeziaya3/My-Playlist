bb161350809cd626311cbf6f16942b6dde2aed4c
Chimezie Anyaoha <Chim@Chimezies-MacBook-Pro.local>
2018-01-08 01:54:47 -0500

    edited app.js in react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem,  } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import Hero from './components/Hero';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Footer from './components/Footer';
+import About from './components/About';
+import Player from './components/Player';
+import Landing from './components/Landing';
+
+
 
 class App extends Component {
+  
+
   render() {
     return (
+    <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar inverse collapseOnSelect className="Navbar">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">CMER Playlist</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav className="Nav">
+            <NavItem><Link to="/">Home</Link></NavItem>
+            <NavItem eventKey={2} ><Link to="/about">About</Link></NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem><Link to="/play">Player</Link></NavItem>
+          </Nav> 
+        </Navbar.Collapse>
+      </Navbar>  
+
+          <main>
+            <Route exact path="/" component={Landing} />
+            <Route path="/play" component={Player} />
+            <Route path="/about" component={About} />
+          </main>
+     
+        
+        <Footer />
       </div>
+      </Router>
     );
   }
 }
